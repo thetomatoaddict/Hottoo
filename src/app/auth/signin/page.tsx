c10f1162226dfffb767a8d14abe2664b2f3a68a1
thetomatoaddict <pooooo_@naver.com>
2023-08-10 01:17:25 +0900

    구글 로그인 리디렉션 오류 수정

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -22,7 +22,7 @@ export default async function SignPage({
   const session = await getServerSession(authOptions);
 
   if (session) {
-    redirect('hottoo.vercel.app');
+    redirect('/');
   }
 
   const providers = (await getProviders()) ?? {};
