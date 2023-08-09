f02b99e110e8ac74368ba06c476979d471c56110
thetomatoaddict <pooooo_@naver.com>
2023-08-10 00:50:25 +0900

    로그인 리다이렉트 오류 수정

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -22,7 +22,7 @@ export default async function SignPage({
   const session = await getServerSession(authOptions);
 
   if (session) {
-    redirect('/');
+    redirect('hottoo.vercel.app');
   }
 
   const providers = (await getProviders()) ?? {};
